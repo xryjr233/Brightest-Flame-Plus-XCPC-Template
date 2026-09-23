b19889b949e0e448e30af6647ddc92434d3a41ce
cmk666 <chen-20071012@foxmail.com>
2026-09-23 19:02:42 +0800

    simplified code

--- src/math/setpoly/exp.cpp
+++ src/math/setpoly/exp.cpp
@@ -1,8 +1,8 @@
 setpoly exp(const setpoly &x) {
 	assert(!x[0]);
 	setpoly y = {1};
-	for (int i = 0, j = 1; j < x.size(); i++, j <<= 1) {
-		setpoly z(x.begin() + j, x.begin() + (j << 1));
+	for (int i = 1; i < x.size(); i <<= 1) {
+		setpoly z(x.begin() + i, x.begin() + (i << 1));
 		z = conv(z, y);
 		y.insert(y.end(), z.begin(), z.end());
 	}
--- src/math/setpoly/ln.cpp
+++ src/math/setpoly/ln.cpp
@@ -1,9 +1,9 @@
 setpoly ln(const setpoly &x) {
 	assert(x[0] == 1);
 	setpoly y = {0};
-	for (int i = 0, j = 1; j < x.size(); i++, j <<= 1) {
-		setpoly z(x.begin(), x.begin() + j);
-		setpoly u(x.begin() + j, x.begin() + (j << 1));
+	for (int i = 1; i < x.size(); i <<= 1) {
+		setpoly z(x.begin(), x.begin() + i);
+		setpoly u(x.begin() + i, x.begin() + (i << 1));
 		z = convinv(z, u);
 		y.insert(y.end(), z.begin(), z.end());
 	}
